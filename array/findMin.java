1212536286108d317547d81bcbf0a82fbc2f473e
praval <pravalvarshney68@gmail.com>
2017-06-20 18:03:04 +0530

    @

---
+++ array/findMin.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class findMin
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=sc.nextInt();
+        int p=0;
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]<a[i-1])
+            {System.out.print(a[i]);p=1;
+            break;}
+            
+        }
+        if(p==0)
+        System.out.print(a[0]);
+    }
+}
